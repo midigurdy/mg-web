2363f31370a4d023fa21aaee3e439934d1f64fb6
Marcus Weseloh <marcus@weseloh.cc>
2018-09-16 15:18:18 +0200

    Cleanup

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,10 +3,7 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
-    //MG_API_URL: '"http://midigurdy.local/api/"',
-    //MG_NOTIFY_WS_URL: '"ws://midigurdy.local:9001"',
-    //MG_DATA_WS_URL: '"ws://midigurdy.local:9000"'
-    MG_API_URL: '"http://192.168.178.133:9090/api/"',
-    MG_NOTIFY_WS_URL: '"ws://192.168.178.133:9001"',
-    MG_DATA_WS_URL: '"ws://192.168.178.133:9000"'
+    MG_API_URL: '"http://localhost:9090/api/"',
+    MG_NOTIFY_WS_URL: '"ws://localhost:9001"',
+    MG_DATA_WS_URL: '"ws://localhost:9000"'
 })
