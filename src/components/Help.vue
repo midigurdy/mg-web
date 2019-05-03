89cb2a113bf26184875c6dc35cc7993167c1bb38
Marcus Weseloh <marcus@weseloh.cc>
2019-05-03 12:06:24 +0200

    Link included and online user manual in web interface

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -4,7 +4,11 @@
     <v-card>
         <v-card-title><h1>Where to get help</h1></v-card-title>
         <v-card-text>
-            <p>NOT MUCH TO SEE HERE YET, MORE CONTENT COMING SOON! In the meantime...</p>
+            <h2>MidiGurdy User Manual</h2>
+            <p><a href="/manual/en/html/index.html" target="_blank">Offline version of the User Manual</a></p>
+            <p><a href="https://midigurdy.com/manual/en/html/index.html" target="_blank">Latest online version of the User Manual</a></p>
+
+            <h2>Other resources</h2>
             <p>For support about all MidiGurdy related topics please use the MidiGurdy Forum at <a href="https://forum.midigurdy.com" target="_blank">forum.midigurdy.com</a>.</p>
             <p>Alternatively, contact information can be found on <a href="http://midigurdy.com" target="_blank">midigurdy.com</a>.</p>
         </v-card-text>
