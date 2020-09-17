27ec97fbccecac024a23602e6da9659a3efb2b88
Marcus Weseloh <marcus@weseloh.cc>
2020-09-17 19:27:48 +0200

    It's alive! (broken) web-interface showing again after deps upgrade

--- config/live.env.js
+++
@@ -1,9 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-    NODE_ENV: '"development"',
-    MG_API_URL: '"http://10.10.10.10/api/"',
-    MG_NOTIFY_WS_URL: '"ws://10.10.10.10:9001"',
-    MG_DATA_WS_URL: '"ws://10.10.10.10:9000"'
-})
