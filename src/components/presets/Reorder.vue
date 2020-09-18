9369db7569b244fc1dc0c4c9899d7b3b85120d78
Marcus Weseloh <marcus@weseloh.cc>
2020-09-18 14:05:30 +0200

    Upgrade to new vuedraggable options syntax

--- src/components/presets/Reorder.vue
+++ src/components/presets/Reorder.vue
@@ -16,7 +16,7 @@
     </mg-toolbar>
 
     <v-list>
-        <draggable v-model="presets" v-if="presets.length" :options="{handle: '.list__tile__action'}">
+        <draggable v-model="presets" v-if="presets.length" handle=".v-list-item__action">
             <v-list-item v-for="preset in presets" :key="preset.id">
                 <v-list-item-action>
                     <v-icon class="drag-pointer" color="pink">reorder</v-icon>
